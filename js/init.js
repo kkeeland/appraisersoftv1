3f4807b496d32d7389f75abcf694cf9470d3ef48
Kevin Keeland <kkeeland@gmail.com>
2017-07-07 00:41:47 -0700

    First Commit - Materialize

---
+++ js/init.js
@@ -0,0 +1,10 @@
+(function($){
+  $(function(){
+
+    $('.button-collapse').sideNav();
+    $('.parallax').parallax();
+    $(".dropdown-button").dropdown();
+     $('.modal').modal();
+
+  }); // end of document ready
+})(jQuery); // end of jQuery name space
