bd50b03ccf91b4f6a619010320a748f8e6f29ab1
Kevin Keeland <kkeeland@gmail.com>
2017-07-09 13:12:23 -0700

    Added Tabs - file notes - modals

--- js/init.js
+++ js/init.js
@@ -5,6 +5,16 @@
     $('.parallax').parallax();
     $(".dropdown-button").dropdown();
      $('.modal').modal();
+      $('select').material_select();
+      $('.datepicker').pickadate({
+    selectMonths: true, // Creates a dropdown to control month
+    selectYears: 15 // Creates a dropdown of 15 years to control year
+  });
+   $('.collapsible').collapsible();
 
   }); // end of document ready
 })(jQuery); // end of jQuery name space
+
+
+
+
