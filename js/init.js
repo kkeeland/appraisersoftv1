e2449d66027f8b5baf2fa9e72e33baf416632e31
Kevin Keeland <kkeeland@gmail.com>
2017-07-09 18:14:27 -0700

    Updated Sub Menu

--- js/init.js
+++ js/init.js
@@ -11,10 +11,31 @@
     selectYears: 15 // Creates a dropdown of 15 years to control year
   });
    $('.collapsible').collapsible();
-
+   $('input.autocomplete, input.shopname').autocomplete({
+    data: {
+      "Susan Dahlquist": null,
+      "Jedediah Dalton": null,
+      "Some Guy": null
+    },
+    limit: 20, // The max amount of results that can be shown at once. Default: Infinity.
+    onAutocomplete: function(val) {
+      // Callback function when value is autcompleted.
+    },
+    minLength: 1, // The minimum length of the input for the autocomplete to start. Default: 1.
+  });
+  $('input.shopname').autocomplete({
+    data: {
+      "Leifs": null,
+      "Elite Collision": null,
+      "ABC Auto Body": null
+    },
+    limit: 20, // The max amount of results that can be shown at once. Default: Infinity.
+    onAutocomplete: function(val) {
+      // Callback function when value is autcompleted.
+    },
+    minLength: 1, // The minimum length of the input for the autocomplete to start. Default: 1.
+  });
   }); // end of document ready
 })(jQuery); // end of jQuery name space
 
-
-
-
+//https://alligator.io/vuejs/lodash-throttle-debounce/
